cb3c3e3155d882ae7c2b8559035549c51c9f6a98
fadli154 <fadlihifziansyah154@gmail.com>
2023-07-10 09:23:29 +0700

    ui table pekerjaan

--- resources/views/admin/pages/data_pekerjaan/data_pekerjaan.blade.php
+++ resources/views/admin/pages/data_pekerjaan/data_pekerjaan.blade.php
@@ -45,39 +45,13 @@
                     <tr>
                         <th>#</th>
                         <th>Nama Pekerjaan</th>
-                        <th>NIK</th>
-                        <th>Agama</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Status Kependudukan</th>
-                        <th>RT</th>
-                        <th>Nomor Telpon</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
-                        <td>Pasya Abinaya Al mala</td>
-                        <td>1050245708900002</td>
-                        <td>Islam</td>
-                        <td class="text-center">
-                            <span class="badge text-bg-primary rounded-circle p-2">
-                                <i class="bi bi-gender-male"></i>
-                            </span>
-                            <span class="badge rounded-circle p-2" style="background-color: rgb(255, 146, 164)">
-                                <i class="bi bi-gender-female"></i>
-                            </span>
-                        </td>
-                        <td class="text-center">
-                            <span class="badge text-bg-success">
-                                Menetap
-                            </span>
-                            <span class="badge text-bg-warning">
-                                Berkunjung
-                            </span>
-                        </td>
-                        <td>1</td>
-                        <td>08887878379</td>
+                        <td>Kuli Bangunan</td>
                         {{-- Tombol Action --}}
                         <td class="">
                             <a class="btn btn-primary dropdown-toggle show" href="#" role="button"
@@ -120,234 +94,24 @@
                         <input type="text" name="peran" value="pekerjaan" hidden>
                         <div class="row">
                             <div class="form-group col">
-                                <label for="nama_lengkap">Nama Lengkap: </label>
+                                <label for="nama_pekerjaan">Nama Pekerjaan : </label>
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                                         <div class="input-group-text ">
-                                            <i class="bi bi-person-fill fs-2"></i>
+                                            <i class="bi bi-briefcase fs-2"></i>
                                         </div>
                                     </div>
                                     <input type="text"
-                                        class="form-control capitalize @error('nama_lengkap') is-invalid @enderror"
-                                        value="{{ old('nama_lengkap') }}" id="nama_lengkap" name="nama_lengkap"
-                                        placeholder="Masukkan Nama Lengkap">
+                                        class="form-control capitalize @error('nama_pekerjaan') is-invalid @enderror"
+                                        value="{{ old('nama_pekerjaan') }}" id="nama_pekerjaan" name="nama_pekerjaan"
+                                        placeholder="Masukkan Nama Pekerjaan">
                                 </div>
-                                @error('nama_lengkap')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                            <div class="form-group col">
-                                <label for="nik">NIK: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-person-vcard-fill fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <input type="text" class="form-control capitalize @error('nik') is-invalid @enderror"
-                                        value="{{ old('nik') }}" id="nik" name="nik"
-                                        placeholder="Masukkan NIK">
-                                </div>
-                                @error('nik')
+                                @error('nama_pekerjaan')
                                     {{ $message }}
                                 @enderror
                             </div>
                         </div>
 
-                        <div class="row">
-                            <div class="form-group col">
-                                <label for="alamat">Alamat: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi bi-geo-alt fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <input type="text"
-                                        class="form-control capitalize @error('alamat') is-invalid @enderror"
-                                        value="{{ old('alamat') }}" id="alamat" name="alamat"
-                                        placeholder="Masukkan Alamat">
-                                </div>
-                                @error('alamat')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                            <div class="form-group col">
-                                <label for="" class="form-label">Agama</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-book-half fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <select class="form-select" name="agama" aria-label="Default select example">
-                                        <option selected>Pilih di bawah ini</option>
-                                        <option value="0">Islam</option>
-                                        <option value="1">Kristen Protestan</option>
-                                        <option value="2">Kristen Katolik</option>
-                                        <option value="3">Khonghucu</option>
-                                        <option value="4">Hindu</option>
-                                        <option value="5">Buddha</option>
-                                    </select>
-                                </div>
-                                @error('agama')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group col">
-                                <label for="tanggal_lahir">Tanggal lahir: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-calendar-event-fill fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <input type="date"
-                                        class="form-control capitalize @error('tanggal_lahir') is-invalid @enderror"
-                                        value="{{ old('tanggal_lahir') }}" id="tanggal_lahir" name="tanggal_lahir"
-                                        placeholder="Masukkan Tanggal lahir">
-                                </div>
-                                @error('tanggal_lahir')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                            <div class="form-group col">
-                                <label for="" class="form-label">Jenis kelamin</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-gender-ambiguous fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <select class="form-select" name="jenis_kelamin" aria-label="Default select example">
-                                        <option selected>Jenis Kelamin</option>
-                                        <option value="L" {{ old('jenis_kelamin') == 'L' ? 'selected' : '' }}>
-                                            Laki - Laki
-                                        </option>
-                                        <option value="P" {{ old('jenis_kelamin') == 'P' ? 'selected' : '' }}>
-                                            Perempuan
-                                        </option>
-                                    </select>
-                                </div>
-                                @error('jenis_kelamin')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group col">
-                                <label for="status_perkawinan">Status Perkawinan: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-postcard-heart-fill fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <select class="form-select" name="status_perkawinan"
-                                        aria-label="Default select example">
-                                        <option selected>Pilih di bawah ini</option>
-                                        <option value="0">Sudah Menikah</option>
-                                        <option value="1">Belum Menikah</option>
-                                        <option value="2">Cerai</option>
-                                    </select>
-                                </div>
-                                @error('status_perkawinan')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                            <div class="form-group col">
-                                <label for="status_kependudukan">Status Kependudukan: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-geo-fill fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <select class="form-select" name="status_kependudukan"
-                                        aria-label="Default select example">
-                                        <option selected>Pilih di bawah ini</option>
-                                        <option value="0">Menetap</option>
-                                        <option value="1">Berkunjung</option>
-                                    </select>
-                                </div>
-                            </div>
-                            @error('status_kependudukan')
-                                {{ $message }}
-                            @enderror
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group col">
-                                <label for="kepekerjaannegaraan">Kepekerjaannegaraan: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-globe-americas fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <select class="form-select" name="kepekerjaannegaraan"
-                                        aria-label="Default select example">
-                                        <option selected>Pilih di bawah ini</option>
-                                        <option value="0">WNI</option>
-                                        <option value="1">WNA</option>
-                                    </select>
-                                </div>
-                                @error('kepekerjaannegaraan')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                            <div class="form-group col">
-                                <label for="nomor_telpon">Nomor Telepon: </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-telephone-fill fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <input type="number"
-                                        class="form-control capitalize @error('nomor_telpon') is-invalid @enderror"
-                                        value="{{ old('nomor_telpon') }}" id="nomor_telpon" name="nomor_telpon"
-                                        placeholder="Masukkan Nomor Telepon">
-                                </div>
-                                @error('nomor_telpon')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row">
-                            <div class="form-group col">
-                                <label for="id_user">Dari RT : </label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <div class="input-group-text ">
-                                            <i class="bi bi-person-square fs-2"></i>
-                                        </div>
-                                    </div>
-                                    <select class="form-select" name="id_user" aria-label="Default select example">
-                                        <option selected>Pilih di bawah ini</option>
-                                    </select>
-                                </div>
-                                @error('id_user')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col"> <label for="formFile" class="form-label">Foto (Opsional)</label>
-                                <input class="form-control" name="foto" type="file" id="formFile"
-                                    onchange="previewImage(event)">
-
-                                <div class="mt-2">
-                                    <img id="preview" src="#" alt="Preview"
-                                        style="max-width: 200px; max-height: 200px; display: none;">
-                                </div>
-                            </div>
-                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger text-white" data-bs-dismiss="modal">Keluar</button>
